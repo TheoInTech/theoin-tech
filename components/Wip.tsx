ba335cd93490fe2ea32668a8152fc6d5b14e0109
Theo Roque <theoroque95@gmail.com>
2023-08-08 20:48:56 +0800

    wip

--- components/Wip.tsx
+++ components/Wip.tsx
@@ -3,9 +3,7 @@ import React from "react";
 const Wip = () => {
   return (
     <section className="font-medium text-lg text-center h-full w-full flex justify-center items-center">
-      i&apos;m a fullstack developer and indie hacker currently working
-      full-time at accenture as an assoc manager developer whilst building side
-      saas projects in emerging tech like ai and blockchain.
+      wip.
     </section>
   );
 };
